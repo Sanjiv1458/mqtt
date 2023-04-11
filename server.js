629858e27d32f4e5968801788e0054003bdfc113
Sanjiv1458 <sanjiv1458@gmail.com>
2023-04-11 16:20:20 +0530

    mqtt

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const mqtt = require('mqtt');
+const WebSocket = require('ws');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const port = 3000;
+
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+const mqtt_server = 'broker.hivemq.com';
+const mqtt_port = 1883;
+const mqtt_topic = 'SENSOR_DATA_4853';
+
+const mqttClient = mqtt.connect(`mqtt://${mqtt_server}:${mqtt_port}`);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function (ws) {
+  console.log('WebSocket connected');
+
+  mqttClient.on('message', async function (topic, message) {
+    if (topic === mqtt_topic) {
+      const payload = message.toString();
+      ws.send(payload);
+    }
+  });
+
+  mqttClient.subscribe(mqtt_topic, function (err) {
+    if (err) {
+      console.log('MQTT subscription failed: ' + err);
+    } else {
+      console.log('MQTT subscription successful');
+    }
+  });
+  ws.on('close', function() {
+    console.log('WebSocket disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
